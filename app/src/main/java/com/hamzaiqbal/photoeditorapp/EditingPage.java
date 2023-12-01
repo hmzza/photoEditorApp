56768c3e5261ac6325b59cb9de61c8dd5576022a
Hamza Iqbal <hamza.cstn@gmail.com>
2023-12-02 01:52:01 +0500

    implemented select image option UI

---
+++ app/src/main/java/com/hamzaiqbal/photoeditorapp/EditingPage.java
@@ -0,0 +1,14 @@
+package com.hamzaiqbal.photoeditorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editing_page);
+    }
+}
