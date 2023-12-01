255d5ab05f4b0710c9e854d01933416bf51804a8
Hamza Iqbal <hamza.cstn@gmail.com>
2023-12-02 03:56:51 +0500

    photo editor started working but app crashing

--- app/src/main/java/com/hamzaiqbal/photoeditorapp/EditingPage.java
+++ app/src/main/java/com/hamzaiqbal/photoeditorapp/EditingPage.java
@@ -3,6 +3,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import com.dsphotoeditor.sdk.activity.DsPhotoEditorActivity;
 import com.dsphotoeditor.sdk.utils.DsPhotoEditorConstants;
@@ -17,12 +19,15 @@ public class EditingPage extends AppCompatActivity {
         setContentView(R.layout.activity_editing_page);
         uri = getIntent().getStringExtra("uri");
         imageView = findViewById(R.id.img);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.expand_in);
+        imageView.startAnimation(animation);
 
         Intent dsPhotoEditorIntent = new Intent(this, DsPhotoEditorActivity.class);
         dsPhotoEditorIntent.setData(Uri.parse(uri));
         dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_OUTPUT_DIRECTORY,"Photo Editor");
         int[] toolsToHide ={DsPhotoEditorActivity.TOOL_ORIENTATION,DsPhotoEditorActivity.TOOL_CROP};
         dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_TOOLS_TO_HIDE, toolsToHide);
+        startActivityForResult(dsPhotoEditorIntent, 200);
     }
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
